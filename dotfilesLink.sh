d80ea3bc1ea205f1a2867e79691880615bcca280
10380r <10380r@MacBook-Pro-5.local>
2018-11-27 15:14:06 +0900

    いい感じになった

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@ ln -sf ~/dotfiles/.vim/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zsh_profile
 ln -sf ~/dotfiles/.vim  ~/.vim
+ln -sf ~/dotfiles/.jupyter  ~/.jupyter
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
