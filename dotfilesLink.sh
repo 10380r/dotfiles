65f9ed2b6b9bbf4bb5c3cc543d1eaf2373e49998
10380r <10380r@gmail.com>
2018-11-29 10:53:31 +0900

    zsh_profile消した

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/zsh_profile
 ln -sf ~/dotfiles/vim  ~/.vim
 ln -sf ~/dotfiles/jupyter  ~/.jupyter
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
