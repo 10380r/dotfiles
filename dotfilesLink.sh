7e74dec71549564896a6322aba6d885c0080c933
Tomiyama <10380r@gmail.com>
2020-01-01 20:03:52 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -sf ~/dotfiles/zsh/zsh_profile ~/.zsh_profile
+ln -sf ~/dotfiles/zsh/zsh_profile ~/.zprofile
 ln -sf ~/dotfiles/vim  ~/.vim
 ln -sf ~/dotfiles/jupyter  ~/.jupyter
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
